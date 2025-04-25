ca5384d1e80db9becc8343d9a44de66927fd2629
A'nsi Lema <ansilema@gmail.com>
2025-04-25 10:05:38 +0200

    Add custom error pages and update configurations
    Added templates for 404 and 500 error pages and integrated them with custom views.

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile_new')
     favorite_city = models.CharField(max_length=64, blank=True)
